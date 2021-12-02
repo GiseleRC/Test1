d1921fc1b72f85ab6aa340b4a230ea8517a27d6d
GiseleRC <rominagisele.ar@gmail.com>
2021-12-02 16:31:55 -0300

    Phase 1- Update 1
    Create To do app with Vue.

---
+++ todolist/src/components/todoapp.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    HelloWorld
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+
+<style scoped>
+
+ 
+</style>
