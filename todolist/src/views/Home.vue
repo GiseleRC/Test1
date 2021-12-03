469704a980974aaafbc6ca63124d5c6394d03665
GiseleRC <rominagisele.ar@gmail.com>
2021-12-03 15:03:47 -0300

    Phase 1- Commit 8
    Create Login 1.1

--- todolist/src/views/Home.vue
+++ todolist/src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <todoapp msg="Welcome to Your Vue.js App"/>
+    el login aqui
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import todoapp from '@/components/todoapp.vue'
+
 
 export default {
   name: 'Home',
   components: {
-    todoapp
+    
   }
 }
 </script>
